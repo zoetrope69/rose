758c1f8e3bd444b8afb771026800cc36b5bded34
Zac Colley <hi@zaccolley.com>
2014-02-01 17:29:28 +0000

    Changed colours back, other tweaks

--- js/build/script.js
+++ js/build/script.js
@@ -4,8 +4,13 @@ function fadeInLogo(){
 	$('.logo').html('R<ul class="petals"><li></li><li></li><li></li></ul>SE');
 
 }
+
+function bodyPadding(){
+	$('body').css('padding-top', $('header').height());
+}
 (function(){
 
+	bodyPadding();
 	fadeInLogo();
 
 })()
--- js/header.js
+++ js/header.js
@@ -4,3 +4,7 @@ function fadeInLogo(){
 	$('.logo').html('R<ul class="petals"><li></li><li></li><li></li></ul>SE');
 
 }
+
+function bodyPadding(){
+	$('body').css('padding-top', $('header').height());
+}
