50da72d2e6d7c605e2481346d04ca61f090e0c91
Zac Colley <hi@zaccolley.com>
2014-04-05 14:16:08 +0100

    Added read time to blog

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ function loaded(){
 	});
 
 	anchorPageScrolling();
+
+	readingTime();
+	
 }
 
 // if anyone pokes into the console display this
@@ -20,5 +23,19 @@ function consoleSleuths(){
 	if(typeof console !== 'undefined'){
 		console.log("Hey, if you find anything broken, please go post an issue: " +
 					"http://github.com/rosedigital/website/issues :¬)");
+	}
+}
+
+// calculate the reading time of a post
+function readingTime(){
+	// if on the blog page
+	if($('.blog').length){
+
+		var wordsPerMinute = 250; // according to http://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
+		var wordCount = $('.blog .post').text().split(" ").length;
+
+		var readingTime = Math.ceil(wordCount / wordsPerMinute);
+
+		$('.blog .top').append('<small>~'+ readingTime +' min read</small>');
 	}
 }
