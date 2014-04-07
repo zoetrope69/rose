b2038977d89315134356f2b31b2ab90ffa44cf9e
Zac Colley <hi@zaccolley.com>
2014-04-07 17:13:20 +0100

    Added feature detection for logo, closes #11

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 // replaces the logo with the spinny petals version
 function logoInit(){
-	$('.logo').removeClass().addClass('logo'); // remove backup img classes
-	$('.logo a').html('R<ul class="petals"><li></li><li></li><li></li></ul>SE'); // add in spinny petals wow
+	if(Modernizr.fontface && Modernizr.csstransforms && Modernizr.borderradius){
+		$('.logo').removeClass().addClass('logo'); // remove backup img classes
+		$('.logo a').html('R<ul class="petals"><li></li><li></li><li></li></ul>SE'); // add in spinny petals wow
+	}
 }
 
 // adds padding to the top of the page to account for the header
