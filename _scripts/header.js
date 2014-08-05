88387f6ba42c659ca71a7bfdb6323b251d334334
Zac Colley <hi@zaccolley.com>
2014-08-05 15:47:56 +0100

    Home page changes

--- _scripts/header.js
+++
@@ -1,6 +0,0 @@
-function sectionResize(){
-	var headerHeight = $('header').outerHeight();
-
-	var sectionHeight = $(window).height() - headerHeight;
-	$('.home section').css('height', sectionHeight);
-}
