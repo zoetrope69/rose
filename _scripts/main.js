bcbc3f4157a2f1fdacf0e7c3f20a590e06842262
Zac Colley <hi@zaccolley.com>
2014-08-23 17:54:02 +0100

    Improved project planner

--- _scripts/main.js
+++ _scripts/main.js
@@ -7,12 +7,46 @@ $(document).ready(function(){
 	readingTime();
 	
 	consoleSleuths();
+
+	mapInit();
 	
 });
+
+function mapInit(){
+	// if the map is on the page
+	if($('#map')){
+
+		var roseOfficeGeo = [50.794315619385124, -1.0954651236534119];
+
+		var map = L.map('map').setView(roseOfficeGeo, 12);
+
+		L.tileLayer('http://{s}.tiles.mapbox.com/v3/zaccolley.jaccno9i/{z}/{x}/{y}.png', {
+			maxZoom: 18
+		}).addTo(map);
+
+		var roseIcon = L.icon({
+			iconUrl: '/img/map-marker.png',
+
+			iconSize:     [48, 48], // size of the icon
+			iconAnchor:   [24, 24], // point of the icon which will correspond to marker's location
+			popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+		});
+
+		var marker = L.marker(roseOfficeGeo, { icon: roseIcon }).addTo(map).bindPopup("<a href='https://goo.gl/maps/4udjl'>Get directions here.</a>");
+
+	}
+}
 
 function projectPlannerForm(){
 
 	if($('.project-planner')){
+
+		$('.project-planner-link').click(function(e){
+			$('.project-planner input[name="name"]').focus();
+
+			// stop default link behaviour
+			e.preventDefault();
+		});
 
 		$('.project-planner__input').blur(function(e){
 			$(this).addClass('project-planner__input--dirty');
